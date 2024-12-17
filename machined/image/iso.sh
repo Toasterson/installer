b2796d63dc42d316c3de2e90ed98caca77b6f64a
Till Wegmueller <toasterson@gmail.com>
2024-12-17 12:10:12 +0100

    Simplify ISO script and rename qemu iso to generic

--- machined/image/iso.sh
+++ machined/image/iso.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #
-# Copyright 2024 Oxide Computer Company
 # Copyright 2024 Till Wegmüller
 #
 
@@ -11,44 +10,7 @@ set -o errexit
 TOP=$(cd "$(dirname "$0")" && pwd)
 . "$TOP/lib/common.sh"
 
-MACHINE=${MACHINE:-generic}
-SERIAL=${SERIAL:-ttya}
-
-UFS=install
-NAME=installer
-ISO_TYPE=
-CONSOLE=$SERIAL
-
-if [[ $SERIAL == vga ]]; then
-	ISO_TYPE='Framebuffer Installer'
-	CONSOLE=text
-else
-	ISO_TYPE="Serial ($SERIAL) Installer"
-fi
-
-while getopts ':N' c; do
-	case "$c" in
-	N)
-		UFS=generic
-		;;
-	\?)
-		printf 'usage: %s [-o OPTE_VER] [-NO]\n' "$0" >&2
-		exit 2
-		;;
-	esac
-done
-
 cd "$TOP"
-
-ARGS=(
-	'-F' "name=$NAME"
-	'-F' "ufs=$UFS"
-	'-F' "iso_type=$ISO_TYPE"
-	'-F' "console=$CONSOLE"
-)
-if [[ $CONSOLE == tty* ]]; then
-	ARGS+=( '-F' 'console_serial' )
-fi
 
 pfexec image-builder \
     build \
@@ -68,8 +30,8 @@ pfexec image-builder \
     build \
     -d "$DATASET" \
     -g installer \
-    -n "qemu-ttya-iso" \
+    -n "generic-iso" \
     -T "$TOP/templates" \
-    -N "installer" \
+    -N "generic" \
     "${NAMEARGS[@]}" \
     "${ARGS[@]}"
