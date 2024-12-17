d50de678899bc38d439e70a3a311a3f00b49cce3
Till Wegmueller <toasterson@gmail.com>
2024-12-17 11:40:58 +0100

    Fixup boot_archive build

--- machined/image/boot_archive.sh
+++ machined/image/boot_archive.sh
@@ -11,36 +11,13 @@ set -o errexit
 TOP=$(cd "$(dirname "$0")" && pwd)
 . "$TOP/lib/common.sh"
 
-MACHINE=${MACHINE:-generic}
-SERIAL=${SERIAL:-ttya}
-
-UFS=install
 NAME=installer
-CONSOLE=$SERIAL
-
-while getopts ':N' c; do
-	case "$c" in
-	N)
-		UFS=generic
-		;;
-	\?)
-		printf 'usage: %s [-o OPTE_VER] [-NO]\n' "$0" >&2
-		exit 2
-		;;
-	esac
-done
 
 cd "$TOP"
 
 ARGS=(
 	'-F' "name=$NAME"
-	'-F' "ufs=$UFS"
-	'-F' "iso_type=$ISO_TYPE"
-	'-F' "console=$CONSOLE"
 )
-if [[ $CONSOLE == tty* ]]; then
-	ARGS+=( '-F' 'console_serial' )
-fi
 
 pfexec image-builder \
     build \
