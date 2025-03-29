e72cb415534af38ea7b3f77525df74a5e8be82dc
Till Wegmueller <toasterson@gmail.com>
2025-03-29 20:54:50 +0100

    Fixup tr invokation
    Signed-off-by: Till Wegmueller <toasterson@gmail.com>

--- machined/image/boot_archive.sh
+++ machined/image/boot_archive.sh
@@ -23,7 +23,7 @@ ARGS=(
 # Build machined and place it into a place to be picked up by the image build
 #
 cargo build --release
-CARGO_TARGET_DIR=$(cargo metadata | jq '.target_directory')
+CARGO_TARGET_DIR=$(cargo metadata | jq '.target_directory' | tr -d \")
 cp ${CARGO_TARGET_DIR}/release/machined templates/files/machined
 
 #
